bcf791db74030307a6bf9361b310c577fc7d6dc5
Soumya7077 <110816458+Soumya7077@users.noreply.github.com>
2024-12-18 12:28:08 +0530

    project setup

---
+++ app/Http/Controllers/users/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+  public function index()
+  {
+    return view('screens.users.user');
+  }
+
+  public function userform()
+  {
+    return view('screens.users.userCreate');
+  }
+}
