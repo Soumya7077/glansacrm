3948153fda1c4553f4d6656916df7763c742964e
SouravB10 <souravbehuria10@gmail.com>
2024-12-19 18:32:06 +0530

    icons

--- resources/views/screens/Enquiry/enquiry.blade.php
+++ resources/views/screens/Enquiry/enquiry.blade.php
@@ -16,9 +16,9 @@
         <form>
           <!-- First Row -->
           <div class="row mb-3">
-            <div class="col-md-6">
+            <div class="col-md-6 mb-3">
               <div class="form-floating form-floating-outline">
-                <input type="text" class="form-control" id="name" placeholder="John Doe" />
+                <input type="text" class="form-control" id="name" placeholder="Name" />
                 <label for="name">Name</label>
               </div>
             </div>
@@ -38,35 +38,34 @@
                   <input type="text" class="form-control" id="email" placeholder="Email" />
                   <label for="email">Email</label>
                 </div>
-                <span class="input-group-text">@example.com</span>
               </div>
             </div>
             <div class="col-md-6">
-                <div class="form-floating form-floating-outline">
-                  <input type="text" class="form-control" id="qualification" placeholder="Qualification" />
-                  <label for="qualification">Qualification</label>
-                </div>
+              <div class="form-floating form-floating-outline">
+                <input type="text" class="form-control" id="qualification" placeholder="Qualification" />
+                <label for="qualification">Qualification</label>
               </div>
+            </div>
           </div>
 
-           <!-- Third Row -->
-           <div class="row mb-3">
+          <!-- Third Row -->
+          <div class="row mb-3">
             <div class="col-md-6">
-                <div class="form-floating form-floating-outline">
-                    <select class="form-control" id="job-post">
-                        <option value="php">PHP</option>
-                        <option value="react">React</option>
-                        <option value="java">Java</option>
-                    </select>
-                    <label for="job-type">Job Post</label>
-                </div>
+              <div class="form-floating form-floating-outline">
+                <select class="form-control" id="job-post">
+                  <option value="php">PHP</option>
+                  <option value="react">React</option>
+                  <option value="java">Java</option>
+                </select>
+                <label for="job-type">Job Post</label>
               </div>
+            </div>
             <div class="col-md-6">
-                <div class="form-floating form-floating-outline">
-                  <input type="text" class="form-control" id="experience" placeholder="Work Experience" />
-                  <label for="experience">Work Experience</label>
-                </div>
+              <div class="form-floating form-floating-outline">
+                <input type="text" class="form-control" id="experience" placeholder="Work Experience" />
+                <label for="experience">Work Experience</label>
               </div>
+            </div>
           </div>
 
           <!-- Fourth Row -->
@@ -78,28 +77,28 @@
               </div>
             </div>
             <div class="col-md-6">
-                <div class="form-floating form-floating-outline">
-                  <input type="text" class="form-control" id="expected-salary" placeholder="Expected Salary" />
-                  <label for="expected-salary">Expected Salary</label>
-                </div>
+              <div class="form-floating form-floating-outline">
+                <input type="text" class="form-control" id="expected-salary" placeholder="Expected Salary" />
+                <label for="expected-salary">Expected Salary</label>
               </div>
+            </div>
           </div>
 
-           <!-- Fourth Row -->
-           <div class="row mb-3">
+          <!-- Fourth Row -->
+          <div class="row mb-3">
             <div class="col-md-6">
               <div class="form-floating form-floating-outline">
                 <input type="file" class="form-control" id="uploadResume" placeholder="Upload Resume" />
                 <label for="current-salary">Upload Resume</label>
               </div>
             </div>
-            <div class="col-md-6">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-              </div>
-          </div>
 
+          </div>
+          <div class="col-md-6">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
           <!-- Submit Button -->
-         
+
         </form>
       </div>
     </div>
