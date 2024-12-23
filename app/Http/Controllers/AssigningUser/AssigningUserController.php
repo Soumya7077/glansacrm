ee7148ebc4840c588a094bacdefe9e389f97cc3d
Soumya7077 <110816458+Soumya7077@users.noreply.github.com>
2024-12-23 10:12:38 +0530

    Pull code

--- app/Http/Controllers/AssigningUser/AssigningUserController.php
+++ app/Http/Controllers/AssigningUser/AssigningUserController.php
@@ -3,14 +3,47 @@
 namespace App\Http\Controllers\AssigningUser;
 
 use App\Http\Controllers\Controller;
+use App\Models\RecruiterAssignsModel;
+use Exception;
 use Illuminate\Http\Request;
 
 class AssigningUserController extends Controller
 {
-    public function assigninguser()
-    {
-        return view('screens.assigninguser.assigninguser');
-    }
+  public function assigninguser()
+  {
+    return view('screens.assigninguser.assigninguser');
+  }
 
+
+  /** ==============================Assigning particular recruiter to a job=========================== */
+  public function assignRecruiterToJob(Request $request)
+  {
+    try {
+
+      $assignRecruiter = RecruiterAssignsModel::create([
+        'JobId' => $request->jobId,
+        'UserId' => $request->userId,
+        'AssignedBy' => $request->assignedBy,
+        'AssignOn' => now(),
+        'created_at' => now(),
+      ]);
+
+      if($assignRecruiter){
+        return response()-> json([
+          'status'=> 'success',
+          'message'=>'Recruiter assigned successfully',
+          'data'=>$assignRecruiter,
+        ], 201);
+      }
+
+    } catch (Exception $e) {
+      return response()->json([
+        'status' => 'error',
+        'message' => 'Something went wrong, ' . $e->getMessage(),
+      ], 500);
+    }
+  }
+
+  /** ==============================Assigning particular recruiter to a job=========================== */
 
 }
