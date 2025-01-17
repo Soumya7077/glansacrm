802ba18bac53b3d6da6f73ec0c4de00c70dda357
unknown <naveennagam6015@gmail.com>
2025-01-17 18:42:25 +0530

    update put method  method in user page

--- resources/views/screens/employer/employer-list.blade.php
+++ resources/views/screens/employer/employer-list.blade.php
@@ -217,7 +217,7 @@
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
 <script>
     $(document).ready(function () {
         $(document).on('click', '.btn-close', function () {
