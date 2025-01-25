a89434f102a216655a2c3265c9e5685782dc18e8
Soumya7077 <110816458+Soumya7077@users.noreply.github.com>
2025-01-25 14:49:18 +0530

    Component create

--- resources/views/screens/users/user.blade.php
+++ resources/views/screens/users/user.blade.php
@@ -236,22 +236,11 @@
 
 <!-- Confirmation Modal -->
 <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="confirmModalLabel">Confirmation</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Are you sure you want to delete this user?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-danger" id="confirmDeleteButton">Delete</button>
-      </div>
-    </div>
-  </div>
+  <x-alert id="alert" title="Delete User" message="Are you sure you want to delete this user?" showCancelButton="true"
+    cancelButtonText="No, Keep User" showConfirmButton="true" confirmButtonText="Yes, Delete"
+    confirmButtonId="deleteUserButton" confirmButtonClass="btn btn-danger" />
 </div>
+
 
 
 
@@ -262,7 +251,9 @@
 
     function fetchUsers() {
 
+
       var table = $('#table').DataTable();
+
 
       $.ajax({
       url: '/api/getuser',
