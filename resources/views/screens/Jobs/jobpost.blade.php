809473f888facec1479cd4d9f15e7b3ba492ea16
SouravB10 <souravbehuria10@gmail.com>
2025-02-06 16:12:14 +0530

    dept modal

--- resources/views/screens/Jobs/jobpost.blade.php
+++ resources/views/screens/Jobs/jobpost.blade.php
@@ -208,6 +208,7 @@
               <option value="" hidden>Gender</option>
               <option value="Male">Male</option>
               <option value="Female">Female</option>
+              <option value="Others">Others</option>
             </select>
             <label for="Gender">Gender</label>
           </div>
